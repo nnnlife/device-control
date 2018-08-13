f2b0d21bc5cad8793ba06de09b3ee05beeff9d1b
Yongwoo Park <yongwoo1.park@lge.com>
2018-08-13 17:03:10 +0900

    initial commit

---
+++ backend/worker.js
@@ -0,0 +1,10 @@
+const {spawnSync} = require('child_process');
+
+function adb_get_micom_connection() {
+    result = spawnSync('adb', ['forward', 'tcp:0', 'tcp:33451']);
+    return parseInt(result['stdout']);
+}
+
+var result = adb_get_micom_connection();
+console.log(`result ${result}`);
+console.log('nan? ' + isNaN(result));
